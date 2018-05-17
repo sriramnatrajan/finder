ce75ff3f6cabe5ce6840a80ad5d1bc0e0578984f
sriramnatrajan10 <sriramnatrajan10@gmail.com>
2018-05-17 17:47:42 +0530

    downloading asynctask

--- app/src/main/java/com/focusmedica/aqrshell/ItemAdapter.java
+++ app/src/main/java/com/focusmedica/aqrshell/ItemAdapter.java
@@ -12,31 +12,25 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
-import java.util.TreeSet;
 
 
 public class ItemAdapter extends BaseAdapter{
     ArrayList<Object> listcontent;
     Context context;
     DataModel content;
-    String url;
+    String url; TextView tv;
     String chapterName;
     String chapterImage,appfolder;
     String chapterImage1;
     private static final int TYPE_ITEM = 0;
     private static final int TYPE_SEPARATOR = 1;
-    private static final int TYPE_MAX_COUNT=TYPE_SEPARATOR+1;
-      ViewHolder viewHolder;
-    private TreeSet<Integer> mSeperatorSet= new TreeSet<Integer>();
-    private ArrayList<String> mData = new ArrayList<String>();
-    private ArrayList<DataModel> str = new ArrayList<DataModel>();
+   // ViewHolder viewHolder;
     private LayoutInflater mInflater;
-
 
     public ItemAdapter(Context context, ArrayList<Object> listcontent ) {
         this.context = context;
         this.listcontent = listcontent;
-        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -55,7 +49,7 @@ public class ItemAdapter extends BaseAdapter{
 
     @Override
     public int getViewTypeCount() {
-        return 2;
+        return 4;
     }
 
     @Override
@@ -68,61 +62,44 @@ public class ItemAdapter extends BaseAdapter{
 
     @Override
     public boolean isEnabled(int position) {
-        return (getItemViewType(position)==TYPE_ITEM);
-    }
-
-    class ViewHolder {
-        private TextView chapter;
-        private ImageView chapter_thumb;
-        private ImageView chapter_arrow;
+        return (getItemViewType(position)== TYPE_ITEM);
     }
 
     @Override
     public View getView(int position, View view, ViewGroup viewGroup) {
-     //   final ViewHolder viewHolder;
         int rowType = getItemViewType(position);
-        viewHolder = new ViewHolder();
-        if (view == null) {
 
+        if (view == null) {
             switch (rowType) {
                 case TYPE_ITEM:
-                    view = mInflater.inflate(R.layout.content_layout, viewGroup,false);
-                    /*viewHolder.chapter = (TextView) view.findViewById(R.id.chapter_tv);
-                    viewHolder.chapter_thumb=(ImageView)view.findViewById(R.id.chapter_thumb_iv);*/
+                    view = mInflater.inflate(R.layout.content_layout, viewGroup, false);
                     break;
                 case TYPE_SEPARATOR:
-                    view = mInflater.inflate(R.layout.list_group, viewGroup,false);
-                    // viewHolder.chapter=(TextView)view.findViewById(R.id.expand_title);
+                    view = mInflater.inflate(R.layout.list_group, viewGroup, false);
+                    tv = (TextView) view.findViewById(R.id.expand_title);
                     break;
             }
-
-
+        }
         switch (rowType){
             case TYPE_ITEM:
-              DataModel  content = (DataModel) getItem(position);
-                viewHolder.chapter = (TextView) view.findViewById(R.id.chapter_tv);
-                viewHolder.chapter_thumb=(ImageView)view.findViewById(R.id.chapter_thumb_iv);
+                DataModel  content = (DataModel) getItem(position);
+                TextView chapter = (TextView) view.findViewById(R.id.chapter_tv);
+                ImageView chapter_thumb = (ImageView) view.findViewById(R.id.chapter_thumb_iv);
                 chapterName=content.getName();
                 chapterImage=content.getValue();
                 chapterImage1=content.getApp_id();
                 appfolder=content.getApp_id();
-                viewHolder.chapter.setText(chapterName);
+                chapter.setText(chapterName);
                 Log.d("TAGG+++",chapterName+","+chapterImage+","+chapterImage1);
-                Glide.with(context).load(chapterImage+"/"+appfolder+"_thumbnail"+".png").into(viewHolder.chapter_thumb);
+                Glide.with(context).load(chapterImage+"/"+appfolder+"_thumbnail"+".png").into( chapter_thumb);
                 break;
             case TYPE_SEPARATOR:
                 TextView mTextView=(TextView)view.findViewById(R.id.expand_title);
                 String titleString = (String)getItem(position);
-                mTextView.setText(titleString);
+                tv.setText(titleString);
                 break;
         }
-            view.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder) view.getTag();
-
-        }
         return view;
-
     }
-
 }
+
--- app/src/main/java/com/focusmedica/aqrshell/utils/Activities/CollectionsActivity.java
+++ app/src/main/java/com/focusmedica/aqrshell/utils/Activities/CollectionsActivity.java
@@ -2,7 +2,6 @@ package com.focusmedica.aqrshell.utils.Activities;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,13 +11,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.focusmedica.aqrshell.AppListHandler;
 import com.focusmedica.aqrshell.DataModel;
-import com.focusmedica.aqrshell.ExpandableAdapter;
 import com.focusmedica.aqrshell.Item2Adapter;
 import com.focusmedica.aqrshell.ItemAdapter;
 import com.focusmedica.aqrshell.PreviewActivity;
@@ -27,8 +24,6 @@ import com.focusmedica.aqrshell.dbHandler.SQLiteHandler;
 import com.focusmedica.aqrshell.dictionary.Firstpage;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 public class CollectionsActivity extends Activity {
     Button btnBack, btnAdd, btnSync;
@@ -38,23 +33,14 @@ public class CollectionsActivity extends Activity {
     ItemAdapter appListAdapter;
     Item2Adapter mItem2Adapter;
     ArrayList<DataModel> chapterList, chapterList2;
-    ArrayList<DataModel> appList;
+    ArrayList<DataModel> appList,appList1,appList3;
     ArrayList<DataModel> appDetails;
     int ik;
-    ArrayList<DataModel> chooseList = new ArrayList<>();
-    ArrayList<DataModel> chooseList2 = new ArrayList<DataModel>();
-    // MyDataBase myDataBase;
     DataModel dataModel, dataModel2;
     AppListHandler appListHandler;
-    DataModel content;
     SQLiteHandler mSqLiteHandler;
-    ExpandableAdapter mExpandableAdapter;
-    ExpandableListView mExpandableListView;
     String a0, a1, a2, a3, a4, a5;
-    int f0;
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
-    Context mContext;
+    String b0,b1,b2,b3,b4,b5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,13 +81,16 @@ public class CollectionsActivity extends Activity {
             a3 = dataModel.getApp_id();
             a4 = dataModel.getApp_type();
             ik = Integer.parseInt(a4);
-            Log.d("@@@@@@@@@@@@@", a0 + a1 + a2 + a3 + a4 + a5);
+            Log.d("@@@@@@@@@@@@@", a0 + a1 + a2 + a3 + a4 );
             appListHandler.addAppList(dataModel);
         }
-        appList = mSqLiteHandler.getAppList();
+        appList = mSqLiteHandler.getUnd("1");
+        appList1=mSqLiteHandler.getUnd("2");
         Log.d("@@@", "size=" + appList.size());
-
+        Log.d("@@@", "appList1=" + appList1.size());
         dataModel = appList.get(0);
+        dataModel2 = appList1.get(0);
+
         a0 = dataModel.getName();
         a1 = dataModel.getValue();
         a2 = dataModel.getAppInfo();
@@ -109,56 +98,50 @@ public class CollectionsActivity extends Activity {
         a4 = dataModel.getApp_type();
         ik = Integer.parseInt(a4);
 
+        b0=dataModel2.getName();
+        b1=dataModel2.getValue();
+        b2=dataModel2.getAppInfo();
+        b3=dataModel2.getApp_id();
+        b4=dataModel2.getApp_type();
+
         chapterList = mSqLiteHandler.getAppList();
         DataModel m1 = new DataModel(a0, a1, a2, a3, a4);
-
+        DataModel m2 = new DataModel(b0, b1, b2, b3, b4);
         ArrayList<DataModel> model = new ArrayList<>();
         model.add(m1);
+        ArrayList<DataModel> m22=new ArrayList<>();
+        m22.add(m2);
+
         ArrayList<DataModel> headerOneList = new ArrayList<>();
         ArrayList<DataModel> headerTwoList = new ArrayList<>();
 
         for (DataModel model1 : model) {
-            if (a4.equals("1")) {
                 headerOneList.add(model1);
-                Log.d("DATATMODEL", "" + a4);
-            } else if (a4.equals("2")){
+                Log.d("DATATMODEL1", "" + headerOneList.size());
+        }
+        for (DataModel model1 : m22) {
                 headerTwoList.add(model1);
-                Log.d("DATATMODEL", "" + a4);
-            }
+                Log.d("DATATMODEL2", "" +  headerTwoList.size());
         }
 
         ArrayList<Object> headerList = new ArrayList<>();
-        headerList.add("header one");
+        headerList.add("Understanding Diseases");
         headerList.addAll(headerOneList);
-        headerList.add("header two");
+        headerList.add("Animated Pocket Dictionary");
         headerList.addAll(headerTwoList);
-        lvAppList.setAdapter(new ItemAdapter(getApplicationContext(), headerList));
-        //chapterList2=mSqLiteHandler.getUnd("2");
-        Log.d("@@@", "chapSize=" + chapterList.size());
+        appListAdapter=new ItemAdapter(getApplicationContext(),headerList);
+        lvAppList.setAdapter(appListAdapter);
+        appListAdapter.notifyDataSetChanged();
+        appList = mSqLiteHandler.getAppList();
 
 
-        //appListAdapter.addSectionHeaderItem("Understanding disease");
-        /*LayoutInflater mLayoutInflater=getLayoutInflater();
-        ViewGroup mViewGroup=(ViewGroup)mLayoutInflater.inflate(R.layout.list_group,lvAppList,false);
-        ViewGroup mViewGroup1=(ViewGroup)mLayoutInflater.inflate(R.layout.list_item,lvAppList,false);
-
-        lvAppList.addHeaderView(mViewGroup);
-        listdict.addHeaderView(mViewGroup1);*/
-
-        //lvAppList.setAdapter(appListAdapter);
-        /*else if (f0==2){
-           appListAdapter.addSectionHeaderItem("Understanding Dictionary");
-           lvAppList.setAdapter(appListAdapter);
-         }*/
-        /*chapterList=mSqLiteHandler.getUnd("2");
-        appListAdapter=new ItemAdapter(CollectionsActivity.this,chapterList);
-        lvAppList.setAdapter(appListAdapter);*/
-
-
+        Log.d("@@@", "appList=" + appList.size());
+        Log.d("@@@", "appList1=" + appList1.size());
         lvAppList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                dataModel = chapterList.get(position);
+                dataModel = appList.get(position);
+
                 String f1 = dataModel.getApp_type();
                 int v1 = Integer.parseInt(f1);
                 Log.d("TAGE", "++++++++" + f1);
